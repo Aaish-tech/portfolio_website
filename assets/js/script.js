b16f0d1a173eef69cd159471850cf16ee14d7d70
Aaish-tech <aaish28siddiqui@gmail.com>
2025-12-18 23:48:46 +0500

    more commits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+function loadPage(baseFile, content) {
+  fetch(baseFile)
+    .then(res => res.text())
+    .then(html => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+
+      const contentDiv = doc.querySelector("#content");
+      if (contentDiv) {
+        contentDiv.innerHTML = content;
+      }
+
+      document.body.innerHTML = doc.body.innerHTML;
+    })
+    .catch(err => console.error("Error loading base:", err));
+}
+
 document.body.style.opacity = 0;
 window.addEventListener("load", () => {
   document.body.style.transition = "opacity 1s ease";
